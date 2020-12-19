63f8c943f95aa6e36d2bf7812c93353164c0b418
최신우 <sinwoo1228@naver.com>
2020-12-20 03:06:38 +0900

    user 응답코드 400번 변경
    - 쿠키에 jwt토큰을 포함시키지않고 jwt인증API(url: /api/user/auth) 호출시 400코드 응답에서 200응답코드(isAuth필드는 false로응답)로 변경

--- user/src/controllers/userController.js
+++ user/src/controllers/userController.js
@@ -4,64 +4,72 @@ import jwt from "jsonwebtoken";
 import { json } from "body-parser";
 import { Op } from "sequelize";
 
-export const getUser = async (req, res) =>{
+export const getUser = async (req, res) => {
 	const { user_ids } = req.query;
 	console.log(req.query);
-	if(!user_ids){
-		return res.status(400).json({ isSuccess: false, message: "잘못된 요청입니다."});
+	if (!user_ids) {
+		return res
+			.status(400)
+			.json({ isSuccess: false, message: "잘못된 요청입니다." });
 	}
 	const userIds = user_ids.split(",").map((e) => Number(e));
-	if(userIds.length <= 0) {
-		return res.json({ isSuccess: true, users: []});
+	if (userIds.length <= 0) {
+		return res.json({ isSuccess: true, users: [] });
 	}
-	
-	try{
+
+	try {
 		const response = await UserModel.findAll({
 			where: {
-				id : {
-					[Op.in] : userIds
-				}
-			}
+				id: {
+					[Op.in]: userIds,
+				},
+			},
 		});
-		return res.json({ isSuccess: true, users: response});
-	} catch(error){
+		return res.json({ isSuccess: true, users: response });
+	} catch (error) {
 		return res.status(500).json({
 			isSuccess: false,
 			message: "서버처리 오류",
 		});
 	}
-
-}
+};
 
 export const getUserById = async (req, res) => {
 	const { id } = req.params;
-	if(!Number(id)){
+	if (!Number(id)) {
 		return res.status(400).json({
 			isSuccess: false,
-			message: "옳바른 요청값이 아닙니다."
-		})
+			message: "옳바른 요청값이 아닙니다.",
+		});
 	}
-	try{
-		const user = await UserModel.findOne({where : {id}});
-		if(!user){
+	try {
+		const user = await UserModel.findOne({ where: { id } });
+		if (!user) {
 			return res.json({
 				isSuccess: false,
 				message: "요청값에 해당하는 유저정보가 없습니다.",
 			});
 		} else {
-			const {id, email, address1, address2, nickname, profile_image_url} = user.dataValues;
+			const {
+				id,
+				email,
+				address1,
+				address2,
+				nickname,
+				profile_image_url,
+			} = user.dataValues;
 			return res.json({
 				isSuccess: true,
-				user : {id, email, address1, address2, nickname, profile_image_url}
-			}); 
+				user: { id, email, address1, address2, nickname, profile_image_url },
+			});
 		}
-	} catch(error){
+	} catch (error) {
 		return res.status(500).json({
 			isSuccess: false,
 			message: "서버처리 오류",
 		});
 	}
-}
+};
 
 export const loginUser = async (req, res) => {
 	const { email, password } = req.body;
@@ -74,14 +82,28 @@ export const loginUser = async (req, res) => {
 			});
 		}
 		if (user.dataValues.password === password) {
-			const {id, email, address1, address2, nickname, profile_image_url} = user.dataValues;
-			const expireTime = Math.floor(Date.now() / 1000) + (60 * 60);
+			const {
+				id,
+				email,
+				address1,
+				address2,
+				nickname,
+				profile_image_url,
+			} = user.dataValues;
+			const expireTime = Math.floor(Date.now() / 1000) + 60 * 60;
 			const token = jwt.sign(
-				{ id, email, exp: expireTime},
+				{ id, email, exp: expireTime },
 				process.env.SECRET_TOKEN
 			);
-			res.cookie("x_auth", token,{ expires: new Date(Date.now() + 1000* 60 * 60), httpOnly:true})
-				.json({ loginSuccess: true, user:{id, email, address1, address2, nickname, profile_image_url} });
+			res
+				.cookie("x_auth", token, {
+					expires: new Date(Date.now() + 1000 * 60 * 60),
+					httpOnly: true,
+				})
+				.json({
+					loginSuccess: true,
+					user: { id, email, address1, address2, nickname, profile_image_url },
+				});
 		} else {
 			res.json({
 				loginSuccess: false,
@@ -94,16 +116,19 @@ export const loginUser = async (req, res) => {
 	}
 };
 
-export const logoutUser = (req,res) => {
+export const logoutUser = (req, res) => {
 	const token = req.cookies.x_auth;
 	if (!token) {
-		return res
-			.status(400)
-			.json({ logoutSuccess: false, code: 400, message: "전송된 토큰이 없습니다." });
+		return res.status(400).json({
+			logoutSuccess: false,
+			code: 400,
+			message: "전송된 토큰이 없습니다.",
+		});
 	}
-	return res.cookie("x_auth", "",{ expires: new Date(Date.now()), httpOnly:true})
-				.json({ logoutSuccess: true });
-}
+	return res
+		.cookie("x_auth", "", { expires: new Date(Date.now()), httpOnly: true })
+		.json({ logoutSuccess: true });
+};
 
 export const joinUser = async (req, res) => {
 	const { email, password, address1, address2, nickname } = req.body;
@@ -159,13 +184,13 @@ export const authUser = (req, res) => {
 	const token = req.cookies.x_auth;
 	if (!token) {
 		return res
-			.status(400)
-			.json({ isAuth: false, code: 400, message: "전송된 토큰이 없습니다." });
+			.status(200)
+			.json({ isAuth: false, code: 200, message: "전송된 토큰이 없습니다." });
 	}
 	try {
 		const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
-		const {id, email} = decoded
-		return res.json({ isAuth: true, code: 200, user:{id, email} });
+		const { id, email } = decoded;
+		return res.json({ isAuth: true, code: 200, user: { id, email } });
 	} catch (error) {
 		if (error.name === "TokenExpiredError") {
 			return json
@@ -198,4 +223,4 @@ export const isExistUser = async (req, res) => {
 		console.log(error);
 		res.status(500).json({ status: 500 });
 	}
-}
+};
