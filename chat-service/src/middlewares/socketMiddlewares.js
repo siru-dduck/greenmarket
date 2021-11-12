82daa1b25ce2e48c3d4b5403a26f0f7477b82d79
shinwoo <sinwoo1225@gmail.com>
2021-11-13 00:32:56 +0900

    socket io 버전업 & socket io jwt 인증방식 수정

--- chat-service/src/middlewares/socketMiddlewares.js
+++ chat-service/src/middlewares/socketMiddlewares.js
@@ -10,12 +10,10 @@ const SERVICE_PRODUCT_PORT =
 
 export const authJwt = (socket, next) => {
 	try {
-		const token = socket.request.cookies.x_auth;
-		console.dir(socket.request);
-		console.log(token.cookies);
-		const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
-		const { id } = decoded;
-		socket.userId = id;
+		const authHeader = socket.handshake.headers.authorization;
+		const token = authHeader.substring("Bearer ".length);
+		const authUser = jwt.verify(token, process.env.SECRET_TOKEN);
+		socket.authUser = authUser;
 		next();
 	} catch (error) {
 		console.error(error);
@@ -30,31 +28,31 @@ export const authJwt = (socket, next) => {
 export const initializeSocket = async (socket, next) => {
 	console.log("init socket", next);
 	try {
-		const response = await axios.get(
-			`http://${SERVICE_PRODUCT_HOST}:${SERVICE_PRODUCT_PORT}/api/products?user_id=${socket.userId}`
-		);
-		const articleIds = response.data.productArticles.map((e) => {
-			return e.id;
-		});
-		const chatRoom = await ChatRoom.findAll({
-			where: {
-				[Op.or]: [
-					{ article_id: { [Op.in]: articleIds } },
-					{ user_id_buyer: socket.userId },
-				],
-			},
-			order: [["article_id"]],
-		});
-		socket.join(`user_${socket.userId}`);
-		chatRoom.forEach((room) => {
-			socket.join(`room_${room.id}`);
-		});
+		// const response = await axios.get(
+		// 	`http://${SERVICE_PRODUCT_HOST}:${SERVICE_PRODUCT_PORT}/api/products?user_id=${socket.userId}`
+		// );
+		// const articleIds = response.data.productArticles.map((e) => {
+		// 	return e.id;
+		// });
+		// const chatRoom = await ChatRoom.findAll({
+		// 	where: {
+		// 		[Op.or]: [
+		// 			{ article_id: { [Op.in]: articleIds } },
+		// 			{ user_id_buyer: socket.userId },
+		// 		],
+		// 	},
+		// 	order: [["article_id"]],
+		// });
+		// socket.join(`user_${socket.userId}`);
+		// chatRoom.forEach((room) => {
+		// 	socket.join(`room_${room.id}`);
+		// });
 
 		next();
 	} catch (error) {
 		console.log(error);
-		const err = new Error("Can Not Found User Chat Inform");
-		err.data = { type: "CanNotFoundUserChatInform" };
+		const err = new Error("Socket Init Fail");
+		err.data = { type: "SocketInitFail" };
 		next(err);
 	}
 };
