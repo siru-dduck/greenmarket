32c06de5f3d03e9346d55acad039ccba0ea32686
shinwoo <sinwoo1225@gmail.com>
2021-11-18 16:20:36 +0900

    front service 프로젝트 초기세팅
    - webflux, webflux security 세팅
    - swagger 세팅

---
+++ front-service/src/main/java/siru/frontservice/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package siru.frontservice.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+
+import java.util.List;
+
+//@Configuration
+public class WebMvcConfig {
+
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(new SecurityArgumentResolver());
+//    }
+
+}
---
+++ front-service/src/main/java/siru/frontservice/controller/AggregateController.java
@@ -0,0 +1,12 @@
+package siru.frontservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequiredArgsConstructor
+public class AggregateController {
+    
+}
